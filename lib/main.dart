79160f8e1930dd8d0d470f3867fe615642987a0a
yahia-safwat <yahiasafwat95@gmail.com>
2024-05-27 18:20:26 +0300

    Implement state management with 'StateProvider'

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final helloWorldProvider = Provider<String>((ref) => 'Hello world');
-
+final counterStateProvider = StateProvider<int>((ref) {
+  return 0;
+});
 void main() {
   // ProviderScope is a widget that stores the state of all the providers we create.
   runApp(const ProviderScope(child: MyApp()));
@@ -36,19 +37,22 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Riverpod Explorer'),
       ),
       body: const Center(
-        child: HelloWorldWidget(),
+        child: CounterWidget(),
       ),
     );
   }
 }
 
-class HelloWorldWidget extends ConsumerWidget {
-  const HelloWorldWidget({super.key});
+class CounterWidget extends ConsumerWidget {
+  const CounterWidget({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // use ref.watch() to get the value of the provider
-    final helloWorld = ref.watch(helloWorldProvider);
-    return Text(helloWorld);
+    final counter = ref.watch(counterStateProvider);
+    return ElevatedButton(
+      child: Text('Value: $counter'),
+      // change the state inside a button callback
+      onPressed: () => ref.read(counterStateProvider.notifier).state++,
+    );
   }
 }
